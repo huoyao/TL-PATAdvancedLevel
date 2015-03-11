26af0b18b15ea00d39fead46978a4f0e315885c3
huoyao <huoyaotl123@126.com>
2015-03-11 10:04:43 +0800

    delete some headers

--- pat_advanced_level/1043a Is It a Binary Search Tree .cpp
+++ pat_advanced_level/1043a Is It a Binary Search Tree .cpp
@@ -1,12 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <iterator>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <cstdio>
-#include <cmath>
-#include <limits>
 using namespace std;
 
 struct node
