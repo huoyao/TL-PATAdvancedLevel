b3d4c7ad97ff0599019cd7a7a7e8f31af0fd3506
huoyao <huoyaotl123@126.com>
2015-03-14 00:39:34 +0800

    1042 shuffling machine

--- pat_advanced_level/1042a Shuffling Machine.cpp
+++ pat_advanced_level/1042a Shuffling Machine.cpp
@@ -11,7 +11,9 @@ int main()
   vector<string> card(55);
   int cnt = 1;
   string cc[4] = {"S","H","C","D"};
-  int order[55];
+  vector<int> order(55);
+  vector<int> order_(55);
+  vector<int> ordertmp(55);
   for (int id = 0; id < 4; ++id)
   {
     for (int i = 1; i < 14; ++i)
@@ -24,23 +26,19 @@ int main()
   card[cnt++] = "J1";
   card[cnt] = "J2";
   for (int i = 1; i < 55; ++i)
-    scanf("%d",&order[i]);
-  //  order[i] = i + 1;
-  //order[54] = 1;
+  {
+    scanf("%d", &order[i]);
+    ordertmp[i] = i;
+  }
   while (rep--)
   {
-    int tp2 = 1,next,cur=order[tp2];
-    for (int i = 0; i < 54; ++i)
-    {
-      next = order[cur];
-      order[cur] = tp2;
-      tp2 = cur;
-      cur=next;
-    }
+    for (int i = 1; i <= 54; ++i)
+      order_[order[i]] = ordertmp[i];
+    ordertmp = order_;
   }
   for (int i = 1; i < 54; ++i)
-    printf("%s ", card[order[i]].c_str());
-  printf("%s\n",card[order[54]].c_str());
+    printf("%s ", card[ordertmp[i]].c_str());
+  printf("%s\n",card[ordertmp[54]].c_str());
   system("pause");
   return 0;
 }
